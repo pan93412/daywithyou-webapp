2462ebc6c1b9c35b2bb68352c6ad8db2dd98c1e2
Yi-Jyun Pan <me@pan93.com>
2025-05-31 12:53:26 +0800

    fix: correct route order

--- routes/api.php
+++ routes/api.php
@@ -7,21 +7,26 @@ use App\Http\Controllers\ApiProductsController;
 use App\Http\Controllers\ApiUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/auth')->group(function () {
-    Route::post('/login', [ApiAuthenticationController::class, 'login'])->name('login');
-    Route::post('/register', [ApiAuthenticationController::class, 'register'])->name('register');
+Route::prefix('/auth')
+    ->name('api.auth.')
+    ->group(function () {
+        Route::post('/login', [ApiAuthenticationController::class, 'login'])->name('login');
+        Route::post('/register', [ApiAuthenticationController::class, 'register'])->name('register');
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [ApiAuthenticationController::class, 'logout'])->name('logout');
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [ApiAuthenticationController::class, 'logout'])->name('logout');
+        });
     });
-})->name('api.auth.');
 
-Route::prefix('/me')->middleware('auth:sanctum')->group(function () {
-    Route::get('/', [ApiUserController::class, 'index']);
-    Route::put('/', [ApiUserController::class, 'update']);
-    Route::put('/password', [ApiUserController::class, 'updatePassword']);
-    Route::delete('/', [ApiUserController::class, 'destroy']);
-})->name('api.me.');
+Route::prefix('/me')
+    ->middleware('auth:sanctum')
+    ->name('api.me.')
+    ->group(function () {
+        Route::get('/', [ApiUserController::class, 'index']);
+        Route::put('/', [ApiUserController::class, 'update']);
+        Route::put('/password', [ApiUserController::class, 'updatePassword']);
+        Route::delete('/', [ApiUserController::class, 'destroy']);
+    });
 
 Route::resource('/news', ApiNewsController::class)
     ->only(['index', 'show'])
